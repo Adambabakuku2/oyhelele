98301965b579b1969d0013067974c1a08d82f837
Adambabakuku2 <156215738+Adambabakuku2@users.noreply.github.com>
2026-02-24 21:28:33 +0300

    Update bot configuration with new token and IDs

--- Config.js
+++ Config.js
@@ -1,17 +1,17 @@
 module.exports = {
   Bot: {
-    Token: "",           // Bot tokeni, Discord geliştirici portalından alınan gizli anahtar.
+    Token: "MTE2MTM0ODA0NDE0MzQ4MDg3Mg.Gc2Ey_.77TG6k5NNViPeoAaTmAFNsntYkiTAJwS1v2-yg",           // Bot tokeni, Discord geliştirici portalından alınan gizli anahtar.
     BotPrefix: ".",      // Botun prefixi, komutlar bu işaretle başlar (örnek: .komut).
-    GuildID: "",         // Botun aktif olacağı Discord sunucusunun ID'si.
-    Owner: ["", "", ""], // Bot sahiplerinin Discord kullanıcı ID'leri, genellikle yetkili kullanıcılar.
+    GuildID: "1448382161538519162",         // Botun aktif olacağı Discord sunucusunun ID'si.
+    Owner: ["1136246184047026186", "", ""], // Bot sahiplerinin Discord kullanıcı ID'leri, genellikle yetkili kullanıcılar.
     BotPresence: [       // Botun durum mesajları, burada çoklu durum tanımlanabilir.
-      "New Private Rooms", // Örnek: Botun Discord'da görünen durumu.
-      ".vodqa <3"          // Diğer durum mesajı.
+      "Kurbağa", // Örnek: Botun Discord'da görünen durumu.
+      "Sikimtr"          // Diğer durum mesajı.
     ],
   },
 
   Emoji: {
-    yes: ":tada:", // Başarılı durumları göstermek için kullanılan emoji (Discord emoji kodu).
-    no: ":tada:",  // Başarısız durumları göstermek için kullanılan emoji (burada aynı emoji, isteğe göre değiştirilebilir).
+    yes: "<:tik:1458075660282105977>", // Başarılı durumları göstermek için kullanılan emoji (Discord emoji kodu).
+    no: "<:hayir:1458076395103322183>",  // Başarısız durumları göstermek için kullanılan emoji (burada aynı emoji, isteğe göre değiştirilebilir).
   }
 }
